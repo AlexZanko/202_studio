948d1ed0b3b8981c437235729a8fc13ba8139f92
AlexZanko <zankovich1992@gmail.com>
2016-12-14 12:29:33 +0300

    Parallax fix

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('section[data-type="background"]').each(function(){
         var $bgobj = $(this); // Назначаем объект
         $(window).scroll(function() {
-            var yPos = -(window.scrollTop() / $bgobj.data('speed'));
+            var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
             // Собираем значение координат фона вместе
             var coords = '50% '+ yPos + 'px';
             // Смещаем фон

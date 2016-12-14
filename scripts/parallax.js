e1f0062139372fb0f435e37a3602634769e9ebce
AlexZanko <zankovich1992@gmail.com>
2016-12-14 12:37:50 +0300

    Parallax IE 9 fix

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
     $('section[data-type="background"]').each(function(){
         var $bgobj = $(this); // Назначаем объект
         $(window).scroll(function() {
-            var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
+            var yPos = -(document.body.scrollTop / $bgobj.data('speed'));
             // Собираем значение координат фона вместе
-            var coords = '50% '+ yPos + 'px';
+            var coords = '50% '+ yPos + 20 + 'px';
             // Смещаем фон
             $bgobj.css({ backgroundPosition: coords });
         });
